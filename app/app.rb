5afa0cab37b43edc0bb12e291d402f43b06541d7
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-04 19:54:37 +0100

    rendered html in sinatra

--- app/app.rb
+++ app/app.rb
@@ -4,8 +4,9 @@ class MakersBnb < Sinatra::Base
   # start the server if ruby file executed directly
   set :public_folder, 'public'
 
-  get '/' do 
-  	send_file 'calendar.html'  	
+  get '/' do
+    File.read(File.join('public', 'calendar.html'))
+  	# send_file 'calendar.html'
   end
 
   run! if app_file == $0
--- public/calendarInterface.js
+++ public/calendarInterface.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  console.log("blah")
 
   $('.date').pickmeup({
   	flat : true,
