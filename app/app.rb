b22c90665ae2281a0c08da6724b863182b307e34
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-04 20:15:28 +0100

    calendar dates now saved in database! woop

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,8 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require 'json'
+require_relative 'booking.rb'
 
 class MakersBnb < Sinatra::Base
   # start the server if ruby file executed directly
@@ -10,6 +13,7 @@ class MakersBnb < Sinatra::Base
   end
 
   post '/reservation' do
+    Booking.create(start_date: params[:start_date], end_date: params[:end_date])
     p params[:start_date]
     p params[:end_date]
   end
