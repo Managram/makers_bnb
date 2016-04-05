632c1de266003a57a881552be42794ccffb55bb7
Patrick <p_donati@hotmail.com>
2016-04-05 11:22:52 +0100

    were doing more stuff

--- app/app.rb
+++ app/app.rb
@@ -12,15 +12,30 @@ class MakersBnb < Sinatra::Base
   end
 
   post '/space/new' do
-    space = Space.create(name: params[:name],
+    @space = Space.create(name: params[:name],
                          description: params[:description],
                          price: params[:price])
     redirect "/space/index"
   end
 
   get '/space/index' do
+    @spaces = Space.all#(name: params[:name],
+    #                     #  description: params[:description],
+    #                     #  price: params[:price])
     erb(:"space/index")
   end
+
+  post '/space/:name/view' do
+    @spaceinfo = Space.get(name: params[:name])
+    p @spaceinfo
+    redirect "/space/view"
+  end
+
+  get '/space/view' do
+    @space = @spaceinfo
+    erb(:"/space/view")
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
