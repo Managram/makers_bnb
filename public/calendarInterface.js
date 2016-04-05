1edd4e32267305d141fd7fd258140f7f4349a2fb
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-05 22:50:54 +0100

    dates grayed out! woop! :D

--- public/calendarInterface.js
+++ public/calendarInterface.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
   function formatDates(dates) {
-    var fomattedDates = [];
+    var formattedDates = new Array();
     $.each(dates, function(index, date){
       var parsedDate = Date.parse(date) - 3600 * 1000;
-      fomattedDates.push(new Date(parsedDate));
+      var newDate = new Date(parsedDate)
+      formattedDates.push(newDate.toString());
     })
-    return fomattedDates;
+    return formattedDates;
   }
 
   $.ajax({
@@ -14,33 +15,24 @@ $(document).ready(function() {
       url:      'http://localhost:4567/booked-dates',
       dataType: 'json',
   }).then(function(data){
-    var bookedDates = data.dates;
-    console.log(typeof bookedDates === "array");
     $('.date').pickmeup({
       flat:   true,
       mode:   'range',
       format: 'Y-m-d',
       render: function(date) {
-        console.log(bookedDates);
-        console.log(date);
-        formatDates(bookedDates);
-
-        console.log(formatDates(bookedDates));
-        console.log(date);
-
-          if ($.inArray(date, bookedDates) > -1){
-            return {
-              disabled   : true,
-              class_name : 'disabled'
-            }
+        var bookedDates = formatDates(data.dates);
+        if ($.inArray(date.toString(), bookedDates) > -1){
+          return {
+            disabled   : true,
+            class_name : 'disabled'
           }
         }
-      });
+      }
     });
+  });
 
   $('#submit').click(function(){
     var dateRange = $('.date').pickmeup('get_date');
-    console.log(dateRange);
     $.ajax({
       type:    'POST',
       url:     'http://localhost:4567/reservation',
@@ -50,14 +42,3 @@ $(document).ready(function() {
     });
   });
 });
-// function formatCalendarDates(date) {
-//     var d = new Date(date),
-//         month = '' + (d.getMonth() + 1),
-//         day = '' + d.getDate(),
-//         year = d.getFullYear();
-//
-//     if (month.length < 2) month = '0' + month;
-//     if (day.length < 2) day = '0' + day;
-//
-//     return [year, month, day].join('-');
-// }
