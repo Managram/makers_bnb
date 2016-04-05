b0a85a1f0bd64dc79e37e4e52934e4e12e2bd845
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-05 14:36:23 +0100

    fixed indentation in caldendarInterface

--- public/calendarInterface.js
+++ public/calendarInterface.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
     var dateRange = $('.date').pickmeup('get_date');
     console.log(dateRange);
     $.ajax({
-        type:        'POST',
-        url:         'http://localhost:4567/reservation',
-        data:        {"start_date": dateRange[0],
-                     "end_date": dateRange[1]},
-        crossDomain: true
+      type:        'POST',
+      url:         'http://localhost:4567/reservation',
+      data:        {"start_date": dateRange[0],
+                   "end_date": dateRange[1]},
+      crossDomain: true
     });
   });
 });
