a4ae888b99e6b507df64a7a9ac8ada851d031a0b
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-06 15:15:38 +0100

    added list a space button in space index. formatted sign up

--- app/app.rb
+++ app/app.rb
@@ -71,7 +71,7 @@ class MakersBnb < Sinatra::Base
                      password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
-      redirect '/space/new'
+      redirect '/space/index'
     else
       flash.now[:errors] = @user.errors.full_messages
       erb(:"user/new")
