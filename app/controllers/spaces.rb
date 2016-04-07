ce8857d24833ccfe7375ef5aa8d263e3911adad9
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-07 12:19:47 +0100

    search by dates workingish

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -13,24 +13,25 @@ class MakersBnb < Sinatra::Base
   end
 
   get '/space/index' do
-    p "there"
-    session[:searched_dates]
     @spaces = Space.all
     @searched_dates = session[:searched_dates]
-    p "there too"
     erb(:"space/index")
   end
 
   post '/space/search' do
     session[:searched_dates] = get_date_range(js_to_rb_date(params[:start_date]), js_to_rb_date(params[:end_date]))
-    p "here"
     # unavailable_dates = retrieve_bookings(Bookings.all)
     redirect "/space/index"
   end
+
+	post '/space/reset-search' do
+		session[:searched_dates] = nil
+		redirect "/space/index"
+	end
 
   get '/space/:id/view' do
     @space = Space.first(id: params[:id])
     erb(:"/space/view")
   end
-  
+
 end
