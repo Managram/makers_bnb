8052dc49af7bcf3d69cdcad7d9a26868a9440f92
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-08 10:07:52 +0100

    Fixed test for listing space

--- spec/features/view_space_spec.rb
+++ spec/features/view_space_spec.rb
@@ -2,7 +2,7 @@ feature 'space page displays information' do
   scenario 'space page displays description and price'do
     register_user
     register_space
-    click_button "view space"
+    click_button "new space"
     expect(page).to have_text "19.99"
   end
 end
