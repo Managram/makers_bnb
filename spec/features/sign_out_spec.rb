ac65a3f6a9fc0f09b9dae905da419b21794b7f51
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-08 10:01:16 +0100

    Fixed user sign out test

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -2,6 +2,6 @@ feature 'user sign out' do
   scenario 'a current user can sign out' do
     register_user
     click_button 'Sign out'
-    expect(page).to have_button 'Sign in'
+    expect(page).to have_content 'Sign In'
   end
 end
